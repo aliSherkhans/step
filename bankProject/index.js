8d4be46ddd0b4f689ed4662d0dd8674553e8c3be
aliSherkhans <sorgaralisher@gmail.com>
2023-08-27 14:31:41 +0530

    bank

--- bankProject/index.js
+++ bankProject/index.js
@@ -1,66 +1,64 @@
-let Pin =1234  // your frist time password
-    let newpin;
-   let cruntbalence = document.getElementById("balance").innerHTML = 1234
+let sbiAccount = {
+      pin: 1234,
+      currentBalance: 5000,
+      name: "Ali sher",
+      accountNumber: 37043555596,
+    };
 
+    document.getElementById("changePin").addEventListener("click", () => {
+      if (validatePin()) {
+        sbiAccount.pin = Number(prompt("what is your new pin"));
+      }
+      
+    });
 
-   function changepin(newpin){
-      Pin = newpin
-   }
-  document.getElementById("changePin").addEventListener("click",()=>{
-    let oldPin = Number(prompt("what is your oldPin"))
-    document.getElementById("password").hidden = true
-    if(Pin == oldPin){
-        newpin = Number(prompt("what is your newpin "))
-        changepin(newpin)
-    }else{
-        alert("plese give a valid pin")
-    }
-  })
+    document.getElementById("whithdrol").addEventListener("click", () => {
+      if (validatePin()) {
+        let amount = Number(prompt("how many amount withdrol"));
+        if (validateCurrentAmount(amount)) {
+          sbiAccount.currentBalance = sbiAccount.currentBalance - amount;
+          showCurrentBalance();
+        }
+        
+      }
+      
+    });
 
+    document.getElementById("deposite").addEventListener("click", () => {
+      if (validatePin()) {
+        let amount = Number(prompt("how many amount deposite"));
+        sbiAccount.currentBalance = sbiAccount.currentBalance + amount;
+        showCurrentBalance();
+      }
+    });
 
- document.getElementById("whithdrol").addEventListener("click",()=>{
-    let askPin = Number(prompt("what is your pin"))
-    let  comparePin = newpin || Pin
-    if(askPin ==  comparePin ){
-    let amount = Number(prompt("how many amount withdrol"))
-    if(cruntbalence >=amount){
-        cruntbalence = cruntbalence - amount
-    document.getElementById("balance").innerHTML = `${cruntbalence}`
-    }else{
+    document.getElementById("transfer").addEventListener("click", () => {
+      if (validatePin()) {
+        let amount = Number(prompt("how many amount transfer"));
+        if (validateCurrentAmount(amount)) {
+          prompt("where are transfer many give a account number");
+          sbiAccount.currentBalance = sbiAccount.currentBalance - amount;
+          showCurrentBalance();
+        }
+      }
+    });
+
+    function validateCurrentAmount(amount) {
+      return (
+        sbiAccount.currentBalance >= amount ||
         alert("etna amunt aapke pass nhi he ")
+      );
     }
-}else{
-    alert("plese give a valid pin ")
-}
- })
 
-
-document.getElementById("deposite").addEventListener("click",()=>{
-    let askPin = Number(prompt("what is your pin"))
-    let  comparePin = newpin || Pin
-    if(askPin ==  comparePin ){
-      let amount = Number(prompt("how many amount deposite"))
-      cruntbalence = cruntbalence + amount
-    document.getElementById("balance").innerHTML = `${cruntbalence}`  
-    }else{
-    alert("plese give a valid pin ")
-}
-})
- 
-document.getElementById("transfer").addEventListener("click",()=>{
-    let askPin = Number(prompt("what is your pin"))
-    let  comparePin = newpin || Pin
-    if(askPin ==  comparePin ){
-    let amount = Number(prompt("how many amount transfer"))
-    if(cruntbalence >=amount){
-        prompt("where are transfer many give a account number")
-        cruntbalence = cruntbalence - amount
-    document.getElementById("balance").innerHTML = `${cruntbalence}`
-    }else{
-        alert("etna amunt aapke pass nhi he ")
+    function validatePin() {
+      let confirmPin = Number(prompt("what is your pin"));
+      return confirmPin == sbiAccount.pin || alert("plese give a valid pin");
     }
-}else{
-    alert("plese give a valid pin ")
-}
-})
 
+    function showCurrentBalance() {
+      document.getElementById(
+        "balance"
+      ).innerHTML = `${sbiAccount.currentBalance};`
+      
+    }
+  
